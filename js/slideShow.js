1dcfce1b6b6e941a5d601597ac64629770047ecb
trantung <tranductung.hhtt@gmail.com>
2022-08-14 08:21:21 +0700

    infinite slider

--- js/slideShow.js
+++ js/slideShow.js
@@ -1,6 +1,4 @@
-function Slider(container){
-    // const arr=['none','black','green','blue','none']
-    const arr=['none','green','blue']
+function Slider(container){ 
     this.container=document.querySelector(container)
     this.slides=this.container.getElementsByClassName("myslide");
     this.dots=this.container.getElementsByClassName("dot");
@@ -10,48 +8,55 @@ function Slider(container){
 
     this.slideTran=this.container.querySelector(".tab-slidemain");
     this.container.style='overflow: hidden'  
+    this.slideItemWidth=this.slides[0].offsetWidth
+    this.slideTran.append(this.slides[0].cloneNode(true))
+    this.slideTran.prepend( this.slides[this.slides.length-1].cloneNode(true))
 
-    let slideItemWidth=this.slides[0].offsetWidth
+    const arr=['black','none','green','black','none']
+    for(let i=0;i<this.slides.length;i++) this.slides[i].style.backgroundColor = arr[i];
 
-    this.fistCLone=this.slides[0].cloneNode(true)
-    this.lastCLone=this.slides[this.slides.length-1].cloneNode(true)
-    // this.slideTran.append(this.fistCLone)
-    // this.slideTran.prepend(this.lastCLone)
-
-    // this.slideTran.style = `transform:translateX(${-(this.slideIndex-1)*slideItemWidth }px)`;
-    console.log(this.slideTran)
     if(this.next || this.prev){
         this.next.onclick=()=>{
-            this.showSlide(this.slideIndex +=1)
+            this.showSlide(this.slideIndex++)     
         }
         this.prev.onclick=()=>{
-            this.showSlide(this.slideIndex -=1)
+            this.showSlide(this.slideIndex--)
         }
     }
     const autoSlide =()=>{
         this.cleartime=setInterval(()=>{
             this.showSlide(this.slideIndex+=1)
-    },4000)
+    },3000)
     }
     for (let i = 0; i < this.dots.length; i++) {
         this.dots[i].addEventListener("click", () => {
           this.showSlide(this.slideIndex=i+1)
         });
     }
-    this.showSlide=(n)=>{  
-        slideItemWidth=this.slides[0].offsetWidth
-        if (n > this.dots.length) this.slideIndex=1
-        else if (n<1) this.slideIndex=this.dots.length
+    this.showSlide=(n)=>{
+        if(this.slideIndex==this.slides.length){    
+            this.slideTran.style = 'transition:none'
+            this.slideIndex=1
+            this.slideTran.style.transform = `translateX(${-this.slideItemWidth * (this.slideIndex)}px)`;
+            this.showSlide(this.slideIndex+=1)
+        }
+        else if(this.slideIndex==0){
+            this.slideTran.style = 'transition:none'
+            this.slideIndex=this.slides.length-1
+            this.slideTran.style.transform = `translateX(${-this.slideItemWidth * (this.slideIndex)}px)`;
+            this.showSlide(this.slideIndex=this.slides.length-2)
+        }    
+        this.slideItemWidth=this.slides[0].offsetWidth
         for (let i = 0; i < this.dots.length; i++)  this.dots[i] = this.dots[i].classList.remove("active-dot");
-        this.slides[this.slideIndex-1].style.backgroundColor = arr[this.slideIndex-1];
-        this.slideTran.style = `transform:translateX(${-(this.slideIndex-1)*slideItemWidth }px)`;
-        // console.log(`transform:translateX(${-(this.slideIndex)*slideItemWidth }px)`)
-        this.dots[this.slideIndex - 1].classList.add('active-dot')
+        this.slideTran.style = `transform:translateX(${-(this.slideIndex)*this.slideItemWidth }px)`;
+        (this.slideIndex==this.slides.length-1 ) ? this.dots[0].classList.add('active-dot') :  this.dots[this.slideIndex -1].classList.add('active-dot')
         clearInterval(this.cleartime)
         autoSlide()
     }
 }
-const bannerSlide1= new Slider('#slide01')
-bannerSlide1.showSlide(1)
-const bannerSlide2= new Slider('#slide02')
-bannerSlide2.showSlide(1)
+const slideIntro= new Slider('#slide-intro')
+slideIntro.showSlide(1)
+const slideProjects= new Slider('#slide-projects')
+slideProjects.showSlide(1)
+
+
